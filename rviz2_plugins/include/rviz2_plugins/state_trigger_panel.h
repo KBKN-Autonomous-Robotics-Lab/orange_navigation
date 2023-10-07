3632cb0fa0171c3d2c0b01cc52c2472ee02cc704
koki.kubota.8p@stu.hosei.ac.jp <koki.kubota.8p@stu.hosei.ac.jp>
2023-10-07 13:17:31 +0000

    Add rviz plugin to start and resume navigation

---
+++ rviz2_plugins/include/rviz2_plugins/state_trigger_panel.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <memory>
+
+#include <QPainter>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QTimer>
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_srvs/srv/trigger.hpp>
+#include <rviz_common/panel.hpp>
+#include <rviz_common/load_resource.hpp>
+
+#include <class_loader/class_loader.hpp>
+
+
+class QPushButton;
+
+namespace rviz2_plugins
+{
+
+class StateTriggerPanel : public rviz_common::Panel
+{
+Q_OBJECT
+public:
+    StateTriggerPanel(QWidget* parent = nullptr);
+    virtual ~StateTriggerPanel();
+
+    virtual void load(const rviz_common::Config& config);
+    virtual void save(rviz_common::Config config) const;
+
+public Q_SLOTS:
+    void pushStartNavigation();
+    void pushResumeNavigation();
+
+private:
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr start_client_;
+    rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr resume_client_;
+    QPushButton *start_nav_button_;
+    QPushButton *resume_nav_button_;
+
+    rclcpp::Node::SharedPtr node_;
+};
+
+}  // namespace rviz2_plugins
